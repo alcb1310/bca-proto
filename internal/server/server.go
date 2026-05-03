6cbf96709dff45134b452789356b46302e4b7fc5
Andres Court <andres@andrescourt.com>
2026-05-03 18:09:24 -0500

    chore(setup): added create server router configuration

---
+++ internal/server/server.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type server struct {
+	port uint16
+}
+
+func New(port string) (http.Server, error) {
+	s := server{}
+	portInt, err := fmt.Sscanf(port, "%d", &s.port)
+	if err != nil {
+		return http.Server{}, err
+	}
+	if portInt != 1 {
+		return http.Server{}, fmt.Errorf("invalid port")
+	}
+
+	return http.Server{
+		Addr:    fmt.Sprintf(":%d", s.port),
+		Handler: s.routes(),
+	}, nil
+}
+
+func (s *server) routes() http.Handler {
+	r := chi.NewRouter()
+
+	return r
+}
