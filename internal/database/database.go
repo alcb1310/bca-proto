57a6517ca81dfb67558a8cfb0cbc123401033742
Andres Court <andres@andrescourt.com>
2026-05-03 20:02:47 -0500

    feat(database): ping database to check its status

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,14 +1,19 @@
 package database
 
 import (
+	"context"
 	"database/sql"
+	"errors"
 	"log/slog"
 	"os"
+	"time"
 
+	"github.com/jackc/pgx/v5/pgconn"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 type Service interface {
+	HealthCheck() error
 }
 
 type service struct {
@@ -29,4 +34,20 @@ func New() Service {
 	}
 
 	return &service{db: db}
+}
+
+func (s *service) HealthCheck() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := s.db.PingContext(ctx)
+	if err != nil {
+		if e, ok := errors.AsType[*pgconn.PgError](err); ok {
+			return e
+		}
+
+		return err
+	}
+
+	return nil
 }
